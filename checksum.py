311deae3a8122471d77b56cfd00f3128183a5a67
David Simowitz <david.simowitz@gmail.com>
2019-06-20 18:25:29 -0400

    refactor: File parsing

--- checksum.py
+++ checksum.py
@@ -4,10 +4,16 @@
 import hashlib
 
 
+def binary_parser(filename):
+    """Parse binary file."""
+    with open(filename, mode='rb') as file:
+        for line in file:
+            yield line
+
+
 def checksum(filename=None, expected=None):
     """Return True if file's checksum matches expected value, else False."""
     hash_obj = hashlib.sha256()
-    with open(filename, mode='rb') as file:
-        for line in file:
-            hash_obj.update(line)
+    for line in binary_parser(filename):
+        hash_obj.update(line)
     return hash_obj.hexdigest() == expected
