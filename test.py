a024db2108d170ee90ad66b471cbf4d8ff5ee2b8
David Simowitz <david.simowitz@gmail.com>
2019-07-08 19:48:17 -0400

    test: Verify result using CLI

--- test.py
+++ test.py
@@ -24,5 +24,26 @@ class TestChecksum(unittest.TestCase):
         )
 
 
+class TestChecksumCli(unittest.TestCase):
+
+    def test_valid_checksum(self):
+        self.assertEqual(
+          checksum.cli(
+            filename='test_file.txt',
+            expected_checksum='b93c5dc9875af15ffe00f5004bd2c13927a522116c40e9a9ed74c127a0df1660'
+          ),
+          'checksums match for file test_file.txt'
+        )
+
+    def test_invalid_checksum(self):
+        self.assertEqual(
+          checksum.cli(
+            filename='test_file.txt',
+            expected_checksum='6a59087a7a99e8670920f2ecdd464e31c4f01cf3d38e8338e5b6c21069cca323'
+          ),
+          'checksums do not match for file test_file.txt'
+        )
+
+
 if __name__ == "__main__":
     unittest.main()
