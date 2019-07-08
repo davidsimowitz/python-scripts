fa8d9040eb43076b85e061a9264ffa4f5cb48317
David Simowitz <david.simowitz@gmail.com>
2019-07-08 19:46:53 -0400

    test: Increase readability

--- test.py
+++ test.py
@@ -8,16 +8,18 @@ import unittest
 class TestChecksum(unittest.TestCase):
 
     def test_valid_checksum(self):
-        self.assertTrue(checksum.checksum(
-          filename='test_file.txt',
-          expected='b93c5dc9875af15ffe00f5004bd2c13927a522116c40e9a9ed74c127a0df1660'
+        self.assertTrue(
+          checksum.checksum(
+            filename='test_file.txt',
+            expected='b93c5dc9875af15ffe00f5004bd2c13927a522116c40e9a9ed74c127a0df1660'
           )
         )
 
     def test_invalid_checksum(self):
-        self.assertFalse(checksum.checksum(
-          filename='test_file.txt',
-          expected='6a59087a7a99e8670920f2ecdd464e31c4f01cf3d38e8338e5b6c21069cca323'
+        self.assertFalse(
+          checksum.checksum(
+            filename='test_file.txt',
+            expected='6a59087a7a99e8670920f2ecdd464e31c4f01cf3d38e8338e5b6c21069cca323'
           )
         )
 
