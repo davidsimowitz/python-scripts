7b803d0a2becdbaeffbb07077b7efaff385972e0
David Simowitz <david.simowitz@gmail.com>
2019-07-15 19:26:56 -0400

    feat: Migrate to Click from argparse
    Click must be installed from PyPI:
    $ pip install click

--- checksum.py
+++ checksum.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 
-import argparse
+import click
 import hashlib
 
 
@@ -11,9 +11,9 @@ def binary_parser(filename):
             yield line
 
 
-def calc_checksum(filename=None):
+def calc_checksum(file=None):
     hash_obj = hashlib.sha256()
-    for line in binary_parser(filename):
+    for line in binary_parser(file):
         hash_obj.update(line)
     return hash_obj.hexdigest()
 
@@ -22,25 +22,18 @@ def checksum(filename=None, expected=None):
     return calc_checksum(filename) == expected
 
 
-def cli(filename=None, expected_checksum=None):
-    if not (filename and expected_checksum):
-        return(f'missing variables(s): '
-               f'{"filename" if not filename else ""}'
-               f'{", " if not (filename or expected_checksum) else ""}'
-               f'{"expected_checksum" if not expected_checksum else ""}')
+@click.command()
+@click.argument('file')
+@click.argument('expected')
+def cli(file, expected):
     try:
-        if checksum(filename, expected_checksum):
-            return f'checksums match for file {filename}'
+        if checksum(file, expected):
+            click.echo(f'checksums match for file {file}')
         else:
-            return f'checksums do not match for file {filename}'
+            click.echo(f'checksums do not match for file {file}')
     except (FileNotFoundError, IsADirectoryError, PermissionError) as err:
-        return f'ERROR: {err}'
+        click.echo(f'ERROR: {err}')
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="verify file's checksum with expected result")
-    parser.add_argument('file', help='file to be verified', type=str)
-    parser.add_argument('expected', help='expected checksum result', type=str)
-    args = parser.parse_args()
-
-    print(cli(args.file, args.expected))
+    cli()
