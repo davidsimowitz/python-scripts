8f5b1296b01cbb2855323adfc0eb2d2b609e7726
David Simowitz <david.simowitz@gmail.com>
2019-07-15 23:14:30 -0400

    test: Blacken code

--- test.py
+++ test.py
@@ -7,37 +7,47 @@ import unittest
 
 
 class TestChecksum(unittest.TestCase):
-
     def test_valid_checksum(self):
         self.assertTrue(
-          checksum.checksum(
-            filename='test_file.txt',
-            expected_checksum=checksum.calc_checksum(filename='test_file.txt')
-          )
+            checksum.checksum(
+                filename="test_file.txt",
+                expected_checksum=checksum.calc_checksum(filename="test_file.txt"),
+            )
         )
 
     def test_invalid_checksum(self):
         self.assertFalse(
-          checksum.checksum(
-            filename='test_file.txt',
-            expected_checksum=checksum.calc_checksum(filename='test_file.txt') + '0'
-          )
+            checksum.checksum(
+                filename="test_file.txt",
+                expected_checksum=checksum.calc_checksum(filename="test_file.txt")
+                + "0",
+            )
         )
 
 
 class TestChecksumCli(unittest.TestCase):
-
     def test_valid_checksum(self):
         cli_runner = click.testing.CliRunner()
-        result = cli_runner.invoke(checksum.cli, args=['test_file.txt', checksum.calc_checksum(filename='test_file.txt')])
+        result = cli_runner.invoke(
+            checksum.cli,
+            args=["test_file.txt", checksum.calc_checksum(filename="test_file.txt")],
+        )
         self.assertEqual(result.exit_code, 0)
-        self.assertEqual(result.output, 'checksums match for file test_file.txt\n')
+        self.assertEqual(result.output, "checksums match for file test_file.txt\n")
 
     def test_invalid_checksum(self):
         cli_runner = click.testing.CliRunner()
-        result = cli_runner.invoke(checksum.cli, args=['test_file.txt', checksum.calc_checksum(filename='test_file.txt') + '0'])
+        result = cli_runner.invoke(
+            checksum.cli,
+            args=[
+                "test_file.txt",
+                checksum.calc_checksum(filename="test_file.txt") + "0",
+            ],
+        )
         self.assertEqual(result.exit_code, 0)
-        self.assertEqual(result.output, 'checksums do not match for file test_file.txt\n')
+        self.assertEqual(
+            result.output, "checksums do not match for file test_file.txt\n"
+        )
 
 
 if __name__ == "__main__":
