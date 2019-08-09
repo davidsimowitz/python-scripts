5f406ef675515b1d33e2e37e93ae142ff7bfc4c8
David Simowitz <david.simowitz@gmail.com>
2019-08-08 20:22:14 -0400

    test: Verify --expected flag

--- test.py
+++ test.py
@@ -21,23 +21,45 @@ class TestChecksum(unittest.TestCase):
 
 
 class TestChecksumCli(unittest.TestCase):
-    def test_valid_checksum(self):
+    def test_valid_calculation(self):
+        cli_runner = click.testing.CliRunner()
+        result = cli_runner.invoke(
+            checksum.cli,
+            args=["test_file.txt",]
+        )
+        self.assertEqual(result.exit_code, 0)
+        self.assertEqual(result.output, "1296b08a46570840ca4fbe5d3a09d2ec35181581484a1b52b826b070d7054fd1\n")
+
+    def test_invalid_calculation(self):
+        cli_runner = click.testing.CliRunner()
+        result = cli_runner.invoke(
+            checksum.cli,
+            args=["test_file.txt",]
+        )
+        self.assertEqual(result.exit_code, 0)
+        self.assertNotEqual(
+            result.output, "1296b08a46570840ca4fbe5d3a09d2ec35181581484a1b52b826b070d7054fd10\n"
+        )
+
+    def test_valid_expected(self):
         cli_runner = click.testing.CliRunner()
         result = cli_runner.invoke(
             checksum.cli,
             args=["test_file.txt",
+                  "--expected",
                   "1296b08a46570840ca4fbe5d3a09d2ec35181581484a1b52b826b070d7054fd1"
             ],
         )
         self.assertEqual(result.exit_code, 0)
         self.assertEqual(result.output, "checksums match for file test_file.txt\n")
 
-    def test_invalid_checksum(self):
+    def test_invalid_expected(self):
         cli_runner = click.testing.CliRunner()
         result = cli_runner.invoke(
             checksum.cli,
             args=[
                 "test_file.txt",
+                "--expected",
                 "1296b08a46570840ca4fbe5d3a09d2ec35181581484a1b52b826b070d7054fd10",
             ],
         )
