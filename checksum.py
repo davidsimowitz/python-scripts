b75171d13fa68d33588e21a3784dd5a0c164f58e
David Simowitz <david.simowitz@gmail.com>
2019-08-08 22:50:53 -0400

    feat: Add additional calculations for checksum

--- checksum.py
+++ checksum.py
@@ -11,8 +11,8 @@ def binary_parser(*, filename=None):
             yield line
 
 
-def checksum(*, filename=None):
-    hash_obj = hashlib.sha256()
+def checksum(*, filename=None, hash_algo="sha256"):
+    hash_obj = eval(f"hashlib.{hash_algo}()")
     for line in binary_parser(filename=filename):
         hash_obj.update(line)
     return hash_obj.hexdigest()
@@ -20,10 +20,11 @@ def checksum(*, filename=None):
 
 @click.command()
 @click.argument("filename")
-@click.option("-e","--expected", "expected_checksum", type=str)
-def cli(*, filename, expected_checksum):
+@click.option("-e","--expected", "expected_checksum", type=str, help="expected checksum value")
+@click.option("-a","--hash-algo", type=click.Choice(['sha1', 'sha224', 'sha256', 'sha384', 'sha512', 'blake2b', 'blake2s']), default="sha256", help="calculation used for checksum")
+def cli(*, filename, expected_checksum, hash_algo):
     try:
-        calculated = checksum(filename=filename)
+        calculated = checksum(filename=filename, hash_algo=hash_algo)
     except (FileNotFoundError, IsADirectoryError, PermissionError) as err:
         msg = f"ERROR: {err}"
     else:
